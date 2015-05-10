450c5e65d99edbe13f50b18615f2a32888357aa0
Marek Sadowski <sadowski.marekk@gmail.com>
2015-05-10 18:41:55 +0200

    cleanup

--- DBLP/src/pl/edu/agh/iiet/mapper/JcrDblpJorunalTitleMatcher.java
+++ DBLP/src/pl/edu/agh/iiet/mapper/JcrDblpJorunalTitleMatcher.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 
